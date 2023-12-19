2af8ec214ebe8d70689bb24b93e84ef63f2a09ba
Ashish <ashish.reg9@gmail.com>
2023-12-19 17:35:35 +0545

    basics of var,let and const

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("Ashish");
+
+//ES5
+var person = "Ashish Regmi";
+person = "Ram Regmi";
+
+//  var overwrites the variable
+
+//ES6
+// Let allows you to reassign the variable
+
+let person2 = "Sita";
+person2 = "Gita";
+
+console.log(person2);
+// prints the variable
+
+//  const refers to constant so it doesnot allow us to change variable
+const person3 = "Harry";
+console.log(person3);
