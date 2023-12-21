7dfd3dcfac93f62dcdb86a24be9cb8a1f0c17df6
Ashish <ashish.reg9@gmail.com>
2023-12-21 08:56:44 +0545

    Switch case

--- operations-statements/statements.js
+++ operations-statements/statements.js
@@ -2,25 +2,60 @@
 // Switch case statements
 
 //calculator
-const a = prompt("Enter the value of a");
-const b = prompt("Enter the value of b");
-const c = prompt("Enter the operator +,-*,/");
-switch (c) {
-  //if(c == +)
-  case "+":
-    let add = parseFloat(a) + parseFloat(b);
-    console.log(add);
+// const a = prompt("Enter the value of a");
+// const b = prompt("Enter the value of b");
+// const c = prompt("Enter the operator +,-*,/");
+// switch (c) {
+//   //if(c == +)
+//   case "+":
+//     let add = parseFloat(a) + parseFloat(b);
+//     console.log(add);
+//     break;
+//   case "-":
+//     console.log(parsefloat(a) - parsefloat(b));
+//     break;
+//   case "*":
+//     console.log(parsefloat(a) * parsefloat(b));
+//     break;
+//   case "/":
+//     console.log(a / b);
+//     break;
+//   default:
+//     console.log("Invalid operator");
+//     break;
+// }
+
+//switch case week
+
+let day = prompt("Enter the day");
+switch (day) {
+  case "Sunday":
+    console.log("We are going to kathamdu in bus");
     break;
-  case "-":
-    console.log(parsefloat(a) - parsefloat(b));
+
+  case "Monday":
+    console.log("We are going to visit Pashupatinath");
     break;
-  case "*":
-    console.log(parsefloat(a) * parsefloat(b));
+
+  case "Tuesday":
+    console.log("We are going to Bhaktapur Durbar Square");
     break;
-  case "/":
-    console.log(a / b);
+
+  case "Wednesday":
+    console.log("We are going to swayambhunath stupa");
     break;
-  default:
-    console.log("Invalid operator");
+
+  case "Thursday":
+    console.log("We are going to Chandragiri hills");
     break;
+
+  case "Friday":
+    console.log("We are going to Visit different places in the valley");
+    break;
+
+  case "Saturday":
+    console.log("We are going to return from kathmandu");
+    break;
+  case "default":
+    console.log("Invalid day");
 }
