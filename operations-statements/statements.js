2aa1e1c3347d7d5d0d8ce7ddc0cdd4213eafc180
Ashish <ashish.reg9@gmail.com>
2023-12-21 17:18:32 +0545

    switch case

--- operations-statements/statements.js
+++ operations-statements/statements.js
@@ -2,22 +2,32 @@
 // Switch case statements
 
 //calculator
-const a = prompt("Enter the value of a");
-const b = prompt("Enter the value of b");
-const c = prompt("Enter the operator +,-*,/");
+const a = parseFloat(prompt("Enter the value of a"));
+const b = parseFloat(prompt("Enter the value of b"));
+const c = prompt("Enter the operator +,-,*,/");
+if (isNaN(a)) {
+  alert("Invalid number");
+}
+
+// if (65 <= a <= 90 && 65 <= b <= 90 && 97 <= a <= 122 && 97 <= b <= 122) {
+//   console.log("please dont enter alphabet");
+// }
 switch (c) {
   //if(c == +)
   case "+":
-    let add = parseFloat(a) + parseFloat(b);
+    let add = a + b;
     console.log(add);
     break;
   case "-":
-    console.log(parsefloat(a) - parsefloat(b));
+    console.log(a - b);
     break;
   case "*":
-    console.log(parsefloat(a) * parsefloat(b));
+    console.log(a * b);
     break;
   case "/":
+    if (a == 0 && b == 0) {
+      console.log("Invalid");
+    }
     console.log(a / b);
     break;
   default:
@@ -27,35 +37,35 @@ switch (c) {
 
 //switch case week
 
-let day = prompt("Enter the day");
-switch (day) {
-  case "Sunday":
-    console.log("We are going to kathmandu in bus");
-    break;
+// let day = prompt("Enter the day");
+// switch (day) {
+//   case "Sunday":
+//     console.log("We are going to kathmandu in bus");
+//     break;
 
-  case "Monday":
-    console.log("We are going to visit Pashupatinath");
-    break;
+//   case "Monday":
+//     console.log("We are going to visit Pashupatinath");
+//     break;
 
-  case "Tuesday":
-    console.log("We are going to Bhaktapur Durbar Square");
-    break;
+//   case "Tuesday":
+//     console.log("We are going to Bhaktapur Durbar Square");
+//     break;
 
-  case "Wednesday":
-    console.log("We are going to swayambhunath stupa");
-    break;
+//   case "Wednesday":
+//     console.log("We are going to swayambhunath stupa");
+//     break;
 
-  case "Thursday":
-    console.log("We are going to Chandragiri hills");
-    break;
+//   case "Thursday":
+//     console.log("We are going to Chandragiri hills");
+//     break;
 
-  case "Friday":
-    console.log("We are going to Visit different places in the valley");
-    break;
+//   case "Friday":
+//     console.log("We are going to Visit different places in the valley");
+//     break;
 
-  case "Saturday":
-    console.log("We are going to return from kathmandu");
-    break;
-  case "default":
-    console.log("Invalid day");
-}
+//   case "Saturday":
+//     console.log("We are going to return from kathmandu");
+//     break;
+//   case "default":
+//     console.log("Invalid day");
+// }
