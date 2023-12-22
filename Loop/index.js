d4347cdf4dedcb0240e001beb50f511c5fe293c5
Ashish <ashish.reg9@gmail.com>
2023-12-22 12:52:47 +0545

    for loop

---
+++ Loop/index.js
@@ -0,0 +1,12 @@
+//for loop to print for 1 to 100
+
+// for (let i = 1; i <= 100; i++) {
+//   console.log(i);
+// }
+
+//Create a multiplication Table for user input.
+
+let num = prompt("Enter the number for multiplication table:");
+for (let a = 1; a <= 10; a++) {
+  console.log(num + "x" + a + "=" + num * a);
+}
