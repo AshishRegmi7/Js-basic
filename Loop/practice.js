e9b983f0b15fec3c8e5238dade865261e4f30574
Ashish <ashish.reg9@gmail.com>
2023-12-22 14:14:40 +0545

    practice

---
+++ Loop/practice.js
@@ -0,0 +1,16 @@
+// Write a JavaScript program to check whether a given positive number is a multiple of 3 or 7.
+let num = number;
+if (num>0){
+ num = prompt("Enter any positive number");
+}
+else{
+    console.log(num +  "isnot a positive number");
+}
+if (num % 3 == 0 || num % 7 == 0) {
+  console.log("Given positive number is a multiple of 3 or 7");
+} else {
+  console.log("not a multiple of 3 or 7");
+}
+else{
+    console.log("Enter a positive number");
+}
