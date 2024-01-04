0e043a19d19aad88b714c7930402fa277072803d
Ashish <ashish.reg9@gmail.com>
2024-01-04 18:05:09 +0545

    practice

--- Array/practice.js
+++ Array/practice.js
@@ -11,13 +11,47 @@ console.log(alpha("ashish"));
 // /*Eg Test
 // 121,-10,10  */
 
-const str = String(prompt("Enter a word or number:"));
+/*const str = String(prompt("Enter a word or number:"));
 const checkPalindrome = (word) => {
   return word.split("").reverse().join("");
 };
 let a = checkPalindrome(str);
 if (str === a) {
-  console.log("Entered word or number is palindrome");
-} else {
-  console.log("Not a palindrome");
+  console.log(str + " is palindrome");
 }
+{
+  console.log(str + " Not a palindrome");
+}
+*/
+//Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word into upper case.
+
+const arr = ["the", "quick", "brown", "fox"];
+const d = arr.map((word) => {
+  return word.toUpperCase();
+});
+console.log(d);
+
+//Write a function that returns username who has admin access.
+const array = [
+  { name: "Ashish", roles: ["admin"] },
+  { name: "Ram", roles: ["user"] },
+  { name: "sita", roles: ["user", "admin"] },
+];
+
+const checkRole = (role) => {
+  const d = array.map((user) => {
+    if (user.roles.includes(role)) return user.name;
+  });
+};
+const result = checkRole("user");
+console.log(result);
+
+//Write a function that sum every character of [1,2,3]
+const f = [1, 2, 3];
+const n = (s) => {
+  const fs = f.reduce((a, b) => {
+    return a + b;
+  }, 0);
+  return fs;
+};
+console.log(n(f));
