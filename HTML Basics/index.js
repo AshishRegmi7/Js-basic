8aa10a14d2b65790e4b14e91fd78e67fb627e076
Ashish <ashish.reg9@gmail.com>
2024-01-09 12:29:59 +0545

    Form bootstrap

--- HTML Basics/index.js
+++ HTML Basics/index.js
@@ -1,10 +1,14 @@
-function today() {
-  return new Date().toISOString();
-}
-document.getElementById("first").innerHTML = today();
-
 function getFormData() {
-  const fName = document.getElementById("fName").value;
-  const lName = document.getElementById("lName").value;
-  console.log(fName, lName);
+  const fName = document.getElementById("fname").value;
+  const lName = document.getElementById("lname").value;
+  const email = document.getElementById("inputEmail4").value;
+  const password = document.getElementById("inputPassword4").value;
+  const address = document.getElementById("inputAddress").value;
+  console.log(
+    "First Name:" + fName,
+    "Last Name:" + lName,
+    "Email Address:" + email,
+    "Password:" + password,
+    "Address:" + address
+  );
 }
